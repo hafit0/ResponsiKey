5e0bc2655b85c0688cfe26d7c57879788e074bf2
Hafit Abekrori <61793573+hafit0@users.noreply.github.com>
2020-12-13 03:53:44 +0700

    Add files via upload

---
+++ ResponsiKey/Controller/MainWindowController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using responsiKey.Model;
+
+namespace responsiKey.Controller
+{
+    class MainWindowController 
+    {
+        KeranjangBelanja keranjangBelanja;
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja)
+        {
+            this.keranjangBelanja = keranjangBelanja;
+        }
+
+        public void addItem(Item item)
+        {
+            this.keranjangBelanja.addItem(item);
+        }
+        public List<Item> getSelectedItems()
+        {
+            return this.keranjangBelanja.getItems();
+        }
+
+        public void deleteSelectedItem(Item item)
+        {
+            this.keranjangBelanja.removeItem(item);
+        }
+
+
+    }
+
+}
---
+++ ResponsiKey/Controller/PenawaranController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using responsiKey.Model;
+
+namespace responsiKey.Controller
+{
+    class PenawaranController
+    {
+        private List<Item> items;
+
+        public PenawaranController()
+        {
+            items = new List<Item>();
+        }
+
+        public void addItem(Item item)
+        {
+            this.items.Add(item);
+        }
+
+        public List<Item> getItems()
+        {
+            return this.items;
+        }
+    }
+}
